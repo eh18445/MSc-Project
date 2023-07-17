b22e183fb7618c1896f6fb24b5440c4ab0274074
eh18445 <danieljohnstoneuk@gmail.com>
2023-07-17 13:03:38 +0100

    Update DFT_test.py

--- PINN_tutorial/DFT_test.py
+++ PINN_tutorial/DFT_test.py
@@ -6,6 +6,17 @@ Created on Sun Jul 16 14:23:56 2023
 """
 
 #Given an atom perform DFT calculations
+#using torch-dftd
 
+from ase.build import molecule
+from torch_dftd.torch_dftd3_calculator import TorchDFTD3Calculator
 
+atoms = molecule("CH3CH2OCH3")
+# device="cuda:0" for fast GPU computation.
+calc = TorchDFTD3Calculator(atoms=atoms,device="cpu",damping="bj")
 
+energy = atoms.get_potential_energy()
+forces = atoms.get_forces()
+
+print(f"energy {energy} eV")
+print(f"forces {forces}")
