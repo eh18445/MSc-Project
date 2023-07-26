bad3f18a61eb40edd28a9cadddb78c760076045f
eh18445 <danieljohnstoneuk@gmail.com>
2023-07-26 17:16:16 +0100

    Update LCAO.py

--- LCAO.py
+++ LCAO.py
@@ -8,6 +8,7 @@ Created on Tue Jul 18 13:12:18 2023
 #Create function that outputs the LCAO solutions for different atomic or molecular setups
 
 #First take the inputs of 2 atoms
+#input is r(position) and R output is Psi_LCAO
 
 #sum the overlapping orbitals together to get the outermost bonding
 
@@ -15,6 +16,7 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
+import time
 
 font = {'size': 11}
 matplotlib.rc('font', **font)
@@ -89,6 +91,8 @@ def atomicUnit(x,y,z,Rx,Ry,Rz,Z):
     Returns the hydrogen atomic orbitals for the atom (in an array).
     """
     
+    time_start = time.time()
+    
     #convert cartesian co-ordinates to polar
     #calculate the orbital
     #convert back to cartesian
@@ -96,22 +100,53 @@ def atomicUnit(x,y,z,Rx,Ry,Rz,Z):
     #cartesian translation and scaling
     x1 = x - Rx
     y1 = y - Ry
-    z1 = z - Rz  
-
-    rVec1 = torch.cat((x1,y1,z1),1)
+    z1 = z - Rz
+    
+    #print(x1)
+    
+    #vector pointing to each co-ordinate
+    p = 0
+    #rVec0 = torch.tensor([])
+    phiVec = torch.zeros((len(x1),len(y1),len(z1)))
+    for i in range(len(x1)):
+        for j in range(len(y1)):
+            for k in range(len(z1)):
+                rVec1 = torch.tensor([[[x1[i]],[y1[j]],[z1[k]]]])
+                
+                r1 = toR(rVec1)
+                
+                #covert to polar co-ords
+                theta1 = torch.arccos(z1[k]/r1)
+                phi1 = torch.sgn(y1[j])*torch.arccos(x1[i]/(torch.pow(x1[i],2)+torch.pow(y1[j],2)))
+                
+                phiVec[i,j,k] = orbital(r1,theta1,phi1,Z,orbital_name='1s')
+                
+                p += 1
+                if (p+1) % 4000 == 0:
+                    print(i,j,k)
+                    print(r1,theta1,phi1)
+                    print(phiVec[i,j,k])    
+                
+                
+    #rVec1 = torch.cat((x1,y1,z1),1)
     #print(rVec1)
-
-    r1 = toR(rVec1)
+    #print(rVec0)
     
-    theta1 = torch.arccos(z1/r1)
-    phi1 = torch.sgn(y1)*torch.arccos(x1/(torch.pow(x1,2)+torch.pow(y1,2)))
+    #convert to radius
+    #r1 = toR(rVec1)
+    
+    #covert to polar co-ords
+    #theta1 = torch.arccos(z1/r1)
+    #phi1 = torch.sgn(y1)*torch.arccos(x1/(torch.pow(x1,2)+torch.pow(y1,2)))
         
-    print(theta1)
-    print(phi1)
+    #print(theta1)
+    #print(phi1)
     
-    phi_r1 = orbital(r1,theta1,phi1,Z,orbital_name='1s')
+    #phi_r1 = orbital(r1,theta1,phi1,Z,orbital_name='1s')
     
-    return phi_r1
+    print('LCAO wavefuncton calclated in:',time.time()-time_start)
+    
+    return phiVec
 
 #Rx, Ry and Rz are positions of atom
 Rx1 = -4
